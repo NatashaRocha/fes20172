f64eb1ba0dc508f89fe316bf664caa9e7550f440
Natasha Rocha <nattycdb@hotmail.com>
2017-11-22 15:08:46 -0200

    Removed Google Calendar + Minor Fixes

---
+++ provas.php
@@ -0,0 +1,42 @@
+[
+    {
+        "title": "P2 de Banco de Dados",
+        "start": "2017-11-13 08:00:00"
+    },
+    {
+        "title": "P2 de Algoritmos e Grafos",
+        "start": "2017-11-23 10:00:00"
+    },
+    {
+        "title": "P2 de Estatísitca",
+        "start": "2017-11-28 08:00:00"
+    },
+    {
+        "title": "P3 de Banco de Dados",
+        "start": "2017-11-29 08:00:00"
+    },
+    {
+        "title": "P3 de Programação Linear",
+        "start": "2017-11-29 10:00:00"
+    },
+    {
+        "title": "PF de Algoritmos e Grafos",
+        "start": "2017-11-30 10:00:00"
+    },
+    {
+        "title": "P2 de FES",
+        "start": "2017-11-23 13:00:00"
+    },
+    {
+        "title": "P3 de Estatísitca",
+        "start": "2017-12-05 08:00:00"
+    },
+    {
+        "title": "PF de FES",
+        "start": "2017-12-05 13:00:00"
+    },
+    {
+        "title": "PF de Programação Linear",
+        "start": "2017-12-06 10:00:00"
+    }
+]
